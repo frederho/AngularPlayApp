f70ec7745ed670652e29ca2faa2a12ce38c8dce6
frederho <frederikkholst@gmail.com>
2015-09-30 16:39:43 +0200

    Pulled directives into separate router-view

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 //Angular app is defined here, the dependencies needed are listed in the []. 
 //Make sure to reference to this module without the [].
-angular.module('angularPlay', ['angularPlay.controllers', 'angularPlay.directives']);
+angular.module('angularPlay', ['angularPlay.controllers', 'angularPlay.directives', 'ui.router'])
+	.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('directives');
+		$stateProvider
+			.state('directives', {
+				url: "/directives",
+				templateUrl: '../templates/directives.html',
+			})		
+	}]);
